85bab64b0619b120a5198a4364246ee136baece4
vinnivso <vinnivsob@gmail.com>
2021-07-15 14:35:30 -0300

    Criação de novas pastas react-01,react-02 e react-03 para melhor organização

---
+++ semana5/react-02/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px 10px 10px;
+  margin-bottom: 10px;
+`;
+
+const LineContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
+const CardImage = styled.img`
+  width: 30px;
+  margin-right: 10px;
+`;
+
+function CardPequeno(props) {
+  return (
+    <CardContainer>
+      <LineContainer>
+        <CardImage
+          src={
+            "https://i.pinimg.com/originals/bb/18/bd/bb18bdbbef437b2d50518db5a8292c94.png"
+          }
+        />
+        <p>{props.email}</p>
+      </LineContainer>
+      <LineContainer>
+        <CardImage
+          src={"https://image.flaticon.com/icons/png/512/69/69524.png"}
+        />
+        <p>{props.endereco}</p>
+      </LineContainer>
+    </CardContainer>
+  );
+}
+
+export default CardPequeno;
