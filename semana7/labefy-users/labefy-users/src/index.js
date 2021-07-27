f1452faf9e8e3eb536581442d29e8f323acab9c5
vinnivso <vinnivsob@gmail.com>
2021-07-27 16:37:40 -0300

    labefy-users, s/ desafios

---
+++ semana7/labefy-users/labefy-users/src/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import Registro from "./Registro";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Registro />
+  </StrictMode>,
+  rootElement
+);
