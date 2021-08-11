63571f957f4a4937971e8f5ee21fc9b69b43352f
vinnivso <vinnivsob@gmail.com>
2021-08-11 12:07:14 -0300

    Semana9-aula32 - DONE

--- semana9/pokeapi-hooksuseeffect/src/App.js
+++ semana9/pokeapi-hooksuseeffect/src/App.js
@@ -1,14 +1,48 @@
-import React, {useEffect, useState} from 'react';
-import './styles.css';
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import './styles.css'
+import PokeCard from './components/PokeCard'
 
-export default function App() {
-  const [poke, setPoke] = useState({pokeList:[],pokeName:''})
+const App = () => {
+  //State definition with hooks.
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState('')
+  //Instead of to declare state by state, we can attribute all properties within the object, as demonstrated in Yuzo's class (10/08/2021).
+  useEffect(() =>{
+    axios
+    .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    .then(res => {
+      // função que está setando no estado os 151 pokemons
+      setPokeList(res.data.results );
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, [])
 
   const changePokeName = (e) => {
-    setPoke(poke => ({...poke, pokeName:e.target.value}))
+    setPokeName(e.target.value);
   }
+  
   return (
-    <div></div>
-  )
+    <div className="App">
+      {/* evento onChange chama função toda vez que o usuário
+      escolhe um novo pokemon no dropdown */}
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {/* renderizando a lista de pokemons como opções do select */}
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      {/* expressão booleana que renderiza o componente PokeCard,
+      caso o valor de pokeName, no estado, seja true */}
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </div>
+  );
 }
+export default App
--- semana9/pokeapi-hooksuseeffect/src/components/PokeCard/index.js
+++ semana9/pokeapi-hooksuseeffect/src/components/PokeCard/index.js
@@ -1,7 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
-export default function PokeCard() {
-  return (
-    <div></div>
-  )
+// At this moment, we have to establish props to this functional component. Why? The PokeCard receives inputs from other, he is a child compared to App.js. In this case, he will receive some kind of data from App.js.
+const PokeCard = (props) => {
+  const [pokemon, SetPokemon] = useState({})
+
+  const pegaPokemon = (pokeName) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then(res => {
+        // Keep the infos about pokemons in state.
+        SetPokemon(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() =>{
+    pegaPokemon(props.pokemon)
+  },[props.pokemon])
+  //The dependencies array auto verify if the information have been changed, and if the value was changed he'll execute what is in useEffect.In this case, PegaPokemon(props.pokemon)
+
+    return (
+      <div>
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
+    );
 }
+export default PokeCard
