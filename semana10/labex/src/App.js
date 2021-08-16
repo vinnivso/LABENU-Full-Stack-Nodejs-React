10c9d00934699df4545056a0218080d08bcdbe5f
vinnivso <vinnivsob@gmail.com>
2021-08-16 16:13:18 -0300

    semana10-labex, START

---
+++ semana10/labex/src/App.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+export default function App() {
+  return(
+    <div>Testing</div>
+  )
+}
