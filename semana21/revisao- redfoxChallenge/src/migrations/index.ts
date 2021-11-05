1bbeac1f70705b931a1a82a24460436cfb3c1639
vinnivso <vinnivsob@gmail.com>
2021-11-04 22:21:59 -0300

    Feat: 3/4

--- semana21/revisao- redfoxChallenge/src/migrations/index.ts
+++ semana21/revisao- redfoxChallenge/src/migrations/index.ts
@@ -1,102 +1,58 @@
-const knex = require('knex')
-const dotenv = require('dotenv')
-const path = require('path')
-const XLSX = require('xlsx');
+import { connection } from "../data/connection";
+import { pokemons } from "./PokemonGo";
 
-dotenv.config()
-
-const connection = knex ({
-  client:'mysql',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_SCHEMA,
-    port: 3306,
-    multipleStatements: true
-  }
-})
-
-const filename = __dirname + path.sep + 'PokemonGo.xlsx'
-const workbook = XLSX.readFile(filename);
-const sheetName = (workbook.SheetNames)[0];
-let pokemons = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {defval: ""});
-
-pokemons = [pokemons[0], pokemons[1]];
-
-const main = async() => {
+const main = async () => {
   try {
     await connection.raw(`
-      CREATE TABLE IF NOT EXISTS pokemon_goDefinitive (
-        ROW INT PRIMARY KEY NOT NULL,
-        Name VARCHAR(255) NOT NULL,
-        PokedexNumber INT,
-        ImgName VARCHAR(255),
-        Generation INT,
-        EvolutionStage VARCHAR(255),
-        Evolved INT NOT NULL,
-        FamilyID INT,
-        CrossGen INT NOT NULL,
-        Type1 VARCHAR(255) NOT NULL,
-        Type2 VARCHAR(255),
-        Weather1 VARCHAR(255) NOT NULL,
-        Weather2 VARCHAR(255),
-        STATTOTAL INT NOT NULL,
-        ATK INT NOT NULL,
-        DEF INT NOT NULL,
-        STA INT NOT NULL,
-        Legendary INT NOT NULL,
-        Aquireable INT NOT NULL,
-        Spawns INT NOT NULL,
-        Regional INT NOT NULL,
-        Raidable INT NOT NULL,
-        Hatchable INT NOT NULL,
-        Shiny INT NOT NULL,
-        Nest INT NOT NULL,
-        New INT NOT NULL,
-        NotGettable INT NOT NULL,
-        FutureEvolve INT NOT NULL,
-      )
-    `)
-    console.log(`Tabela criada!`)
+      CREATE TABLE IF NOT EXISTS redfox_pokemonGo (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+          Row_Value VARCHAR(255),
+          Name VARCHAR(255),
+          Pokedex_Number VARCHAR(255),
+          Img_name VARCHAR(255),
+          Generation VARCHAR(255),
+          Evolution_Stage VARCHAR(255),
+          Evolved VARCHAR(255),
+          FamilyID VARCHAR(255),
+          Cross_Gen VARCHAR(255),
+          Type1 VARCHAR(255),
+          Type2 VARCHAR(255),
+          Weather1 VARCHAR(255),
+          Weather2 VARCHAR(255),
+          STAT_TOTAL VARCHAR(255),
+          ATK VARCHAR(255),
+          DEF VARCHAR(255),
+          STA VARCHAR(255),
+          Legendary VARCHAR(255),
+          Aquireable VARCHAR(255),
+          Spawns VARCHAR(255),
+          Regional VARCHAR(255),
+          Raidable VARCHAR(255),
+          Hatchable VARCHAR(255),
+          Shiny VARCHAR(255),
+          Nest VARCHAR(255),
+          New VARCHAR(255),
+          NotGettable VARCHAR(255),
+          Future_Evolve VARCHAR(255),
+          CP40 VARCHAR(255),
+          CP39 VARCHAR(255)
+      );
+   `);
 
-    for (const pokemon of pokemons) {
-      await connection('pokemon_goDefinitive')
-        .insert({
-          ROW: pokemon.row,
-          Name: pokemon.name,
-          PokedexNumber: pokemon.pokedexNumber,
-          ImgName: pokemon.imageName,
-          Generation: pokemon.generation,
-          EvolutionStage: pokemon.evolutionStage,
-          Evolved: pokemon.evolved,
-          FamilyID: pokemon.familyId,
-          CrossGen: pokemon.crossGen,
-          Type1: pokemon.type1,
-          Type2: pokemon.type2,
-          Weather1: pokemon.weather1,
-          Weather2: pokemon.weather2,
-          STATTOTAL: pokemon.stattotal,
-          ATK: pokemon.atk,
-          DEF: pokemon.def,
-          STA: pokemon.sta,
-          Legendary: pokemon.legendary,
-          Aquireable: pokemon.aquireable,
-          Spawns: pokemon.spawns,
-          Regional:pokemon.regional,
-          Raidable: pokemon.raidable,
-          Hatchable: pokemon.hatchable,
-          Shiny: pokemon.shiny,
-          Nest: pokemon.nest,
-          New: pokemon.new,
-          NotGettable:pokemon.notGettable,
-          FutureEvolve: pokemon.futureEvolve,
-        })
-    }
+    console.log("Tabela criada!");
+
+    await connection("redfox_pokemonGo").insert(
+      pokemons.map((element: any) => {
+        element.Row_Value = element.Row;
+        delete element.Row;
+        return element;
+      })
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   } finally {
-    connection.destroy()
+    connection.destroy();
   }
-}
+};
+
 main();
